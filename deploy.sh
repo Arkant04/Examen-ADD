89934e622f9d959e2e7c9d2feb356291fb075e76
ale <delacorte19@gmail.com>
2025-03-10 11:38:47 +0100

    #update

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "conectando al servidor remoto..."
+
+apt update
+
+apt install npm nodejs certbot 
+
+npm install
+
+# Generar certificados SSL usando certbot
+certbot certonly --standalone -d dev4.cyberbunny.online:3000 --non-interactive --agree-tos -m delacorte19@gmail.com
+
+# Copiar los certificados generados a la ubicación esperada por tu aplicación
+cp /etc/letsencrypt/live/dev4.cyberbunny.online/fullchain.pem 
+cp /etc/letsencrypt/live/dev4.cyberbunny.online/privkey.pem 
+
+echo "proceso completado"
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const https = require('https');
+const http = require('http');
 const cors = require('cors');
-const app = express();
-const port = 3000;
 const fs = require('fs');
 const path = require('path');
-const http = require('http');
+const app = express();
+const port = 3000;
 
 // Middleware para habilitar CORS
 app.use(cors({
@@ -16,15 +16,39 @@ app.use(cors({
 
 // Middleware para parsear el cuerpo de las peticiones en formato JSON
 app.use(express.json());
-app.use(express.static('/public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Endpoint GET / que devuelve un mensaje de bienvenida
 app.get('/', (req, res) => {
   res.json({ message: '¡Bienvenido al servidor!' });
 });
 
+// Configuración del servidor HTTPS
+const httpsOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'certs', 'key.pem')), // Ruta a tu archivo .key
+  cert: fs.readFileSync(path.join(__dirname, 'certs', 'cert.pem')) // Ruta a tu archivo .pem
+};
 
+// Detectar si la solicitud viene desde localhost o VPS
+const isLocal = (req) => {
+  const host = req.hostname; // Obtén el nombre del host
+  return host === 'localhost' || host === '127.0.0.1'; // Verifica si es localhost o IP local
+};
+
+// Crear servidor HTTPS (solo para VPS)
+https.createServer(httpsOptions, app).listen(3443, () => {
+  console.log(`Servidor HTTPS en https://localhost:3443`);
+});
+
+// Crear servidor HTTP (para localhost)
 http.createServer(app).listen(port, () => {
   console.log(`Servidor HTTP en http://localhost:${port}`);
+  // Si no es localhost, redirigir a HTTPS
+  app.use((req, res, next) => {
+    if (!isLocal(req) && req.protocol !== 'https') {
+      // Si no es localhost y la solicitud no es HTTPS, redirigir a HTTPS
+      return res.redirect('https://' + req.headers.host.replace(port, '3443') + req.url);
+    }
+    next();
+  });
 });
