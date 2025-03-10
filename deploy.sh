34d42e8d35ca8c3dc4524bc3c973853a04eae628
ale <delacorte19@gmail.com>
2025-03-10 11:40:00 +0100

    #fix

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ apt install npm nodejs certbot
 npm install
 
 # Generar certificados SSL usando certbot
-certbot certonly --standalone -d dev4.cyberbunny.online:3000 --non-interactive --agree-tos -m delacorte19@gmail.com
+certbot certonly --standalone -d dev4.cyberbunny.online --non-interactive --agree-tos -m delacorte19@gmail.com
 
 # Copiar los certificados generados a la ubicación esperada por tu aplicación
 cp /etc/letsencrypt/live/dev4.cyberbunny.online/fullchain.pem 
